7babc5be5ca546f3d2f422544d38f6521ece985d
StevanSlavnic <slavnic.stevan@gmail.com>
2016-08-11 22:24:44 +0200

    m

--- src/AppBundle/Controller/Message/MessageController.php
+++ src/AppBundle/Controller/Message/MessageController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller\Message;
 
 use AppBundle\Entity\Profile;
 use AppBundle\Entity\User;
+use Elastica\Query\QueryString;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\DependencyInjection\ContainerAware;
